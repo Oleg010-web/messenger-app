5961da56ca24bbb8e49ba1e63f2dd3d87debf705
Oleg010-web <nesteroff_oleg@mail.ru>
2023-03-25 14:46:09 +0700

    Install vue-router, vue-router settings in app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>Welcome, yo</h1>
+  <main class="w-screen h-screen bg-gray-100 flex">
+    <RouterView></RouterView>
+  </main>
+  
 </template>
 
 <script setup lang="ts">
@@ -8,14 +11,6 @@ import { account } from './api'
 import { ID } from 'appwrite'
 
 onMounted(() => {
-  // Register User
-  account.create(ID.unique(), 'me@example.com', 'password', 'Jane Doe').then(
-    response => {
-      console.log(response)
-    },
-    error => {
-      console.log(error)
-    }
-  )
+
 })
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import  Login  from "./pages/Login.vue";
 
-createApp(App).mount('#app')
+const routes: RouteRecordRaw[] = [{path: "/login", component: Login, name: "login"}]
+const router = createRouter({history: createWebHistory(), routes})
+createApp(App).use(router).mount('#app')
