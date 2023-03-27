e366727be5ad67238186a337a5ff1cadaea35fe5
Oleg010-web <nesteroff_oleg@mail.ru>
2023-03-27 22:48:18 +0700

    Started create UI part of sidebar, added new components, decomposible, added new library - heroicons

---
+++ src/components/sidebar/Sidebar.vue
@@ -0,0 +1,7 @@
+<template>
+    <main class="w-[350px] h-full bg-white border-r border-gray-200 flex flex-col">
+        <slot></slot>
+    </main>
+</template>
+
+<script setup lang="ts"></script>
--- src/pages/Application.vue
+++ src/pages/Application.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <h1>Application</h1>
-  </div>
+  <main class="w-full h-full flex">
+     <Sidebar>
+      <ContactsSidebar />
+     </Sidebar>
+     <!-- Main content -->
+  </main>
 </template>
+
+<script setup lang="ts">
+  import ContactsSidebar from '../components/sidebar/ContactsSidebar.vue';
+import Sidebar from '../components/sidebar/Sidebar.vue';
+</script>
