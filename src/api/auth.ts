a96d46d500aea994fd392e6202642aa832d6b7b8
Oleg010-web <nesteroff_oleg@mail.ru>
2023-03-27 20:28:05 +0700

    code correction

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,12 +2,12 @@
 
 import { account } from '.'
 
-// login method
+
 async function auth(email: string, password: string) {
   return account.createEmailSession(email, password)
 }
 
-//Register method
+
 async function registerUser(email: string, password: string, name: string) {
   return account.create('unique()', email, password, name).then(()=>{
     return auth(email, password)
--- src/components/Input.vue
+++ src/components/Input.vue
@@ -14,6 +14,7 @@
 </template>
 
 <script setup lang="ts">
+
 import { ref } from 'vue'
 
 interface Props {
